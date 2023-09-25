f7698382f44aa6711898890ef46b1c3e8d8b9f02
giorgiush <giorgiush8@gmail.com>
2023-09-26 01:16:36 +0900

    create square with 2 triangles using VAO, VBO, EBO

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -33,20 +33,41 @@ int main(void)
 
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    /* Triangle stuff */
-    float positions[6] = {
-        -0.5f, -0.5f,
-        0.0f, 0.5f,
-        0.5f, -0.5f
+    /* Coordinates for two triangles combining into a square */
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, // Bottom left 0
+         0.5f, -0.5f, // Bottom right 1
+         0.5f,  0.5f, // Top right 2
+        -0.5f,  0.5f, // Top left 3 
     };
-    unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(positions[0]), (const void*)positions, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(positions[0]), 0);
+    
+    GLuint indices[] = {
+        0, 1, 3, // Left triangle
+        3, 1, 2 // Right triangle
+    };
+
+    /* Generate and bind VAO */
+    GLuint VAO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    /* Generate and bind VBO then load vertex data into it */
+    GLuint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Generate and bind EBO then load index data into it */
+    GLuint EBO;
+    glGenBuffers(1, &EBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    /* Specify how VAO should interpret the VBO */
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(vertices[0]), (void*)0);
     glEnableVertexAttribArray(0);
 
-    /* Shaders */
+    /* -------------------------Shaders---------------------------- */
     // VERTEX
     const char* vertexShaderSource = R"(#version 330 core
     layout (location = 0) in vec2 aPos;
@@ -71,35 +92,40 @@ int main(void)
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
-    // LINK SHADERS INTO A PROGRAM
+    // Link shaders into a program
     GLuint shaderProgram = glCreateProgram();
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
-    // DELETE SHADERS
+    // Delete shaders
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
+    /* ------------------------------------------------------------ */
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
+        // Clean up back buffer
         glClear(GL_COLOR_BUFFER_BIT);
-
         // Use shader program for rendering
         glUseProgram(shaderProgram);
-
         // Draw call to draw a triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
         /* Poll for and process events */
         glfwPollEvents();
     }
 
+    // Delete objects
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteProgram(shaderProgram);
+    // Delete window
+    glfwDestroyWindow(window);
+    // Terminate GLFW
     glfwTerminate();
     return 0;
 }
